3d4471a7df4850683b2b929bcfcb0d93c3374dcb
darylldan <dccaponpon@up.edu.ph>
2023-03-13 19:45:19 +0800

    feat: added validation checks in addAccount()

--- functions.js
+++ functions.js
@@ -5,4 +5,35 @@ const generateUniqueID = (fName, lName) => {
     return fName.charAt(0).toLowerCase() + lName.toLowerCase() + uuidv4().substring(0, 8);
 }
 
+const addAccount = (infoArr) => {
+    console.log(infoArr)
+    if (!(infoArr.length === 4)) {
+        console.log("Required parameters missing. Account not saved.");
+        return false;
+    }
+
+    if (validator.isEmpty(infoArr[0])) {
+        console.log("First name is empty. Account not saved.");
+        return false;
+    }
+
+    if (validator.isEmpty(infoArr[1])) {
+        console.log("Last name is empty. Account not saved.");
+        return false;
+    }
+
+    if (!(validator.isEmail(infoArr[2]))) {
+        console.log("Invalid email. Account not saved.");
+        return false;
+    }
+
+    if (!(validator.isInt(infoArr[3].toString(), { min: 18}))) {
+        console.log("Did not meet age requirements. Account not saved.");
+        return false;
+    }
+
+    return true;
+}
+
 console.log(generateUniqueID("Alan", "Turing"))
+console.log(addAccount(["Alan", "Turing", "aturing@w3c.com", 58]));
